ba90ce89794fccac1d860c64a1c71624c36c4a85
Bhushan Kalode <bhushan.kalode@loconav.com>
2024-05-15 13:32:26 +0530

    raised negative number error and return 0 if empty input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,9 +1,19 @@
 require_relative '../string_calculator'
 
-describe "String Calculator" do
-  context "Method #add" do
-    it "1, 2 should add to 3" do
-      expect(StringCalculator.add(1, 2)).to eq(3)
+RSpec.describe StringCalculator do
+  describe "Method #add" do
+    context "when numbers string is empty" do
+      it "returns 0" do
+        calculator = StringCalculator.new("")
+        expect(calculator.add).to eq(0)
+      end
+    end
+
+    context "when numbers string contains negative numbers" do
+      it "raises an exception with the negative numbers" do
+        calculator = StringCalculator.new("1,-2,3,-4")
+        expect { calculator.add }.to raise_error("Negative numbers not allowed -> -2, -4")
+      end
     end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,32 @@
 class StringCalculator
-  def self.add(number1, number2)
-    number1 + number2
+  def initialize(numbers)
+    @numbers = numbers
+  end
+
+  # Add multiple numbers provided with deliminator string
+  def add
+    return 0 if @numbers.empty?
+    raise_negative_number
+  end
+
+  # Get custom deliminator if present otherwise return default i.e comma(,)
+  private def deliminator
+    @deliminator ||= custom_deliminator? ? @numbers[2] : ","
+  end
+
+  private def custom_deliminator?
+    @numbers.start_with?("//")
+  end
+
+  private def digits
+    @numbers.gsub("\n", deliminator).split(deliminator).map(&:to_i)
+  end
+
+  private def raise_negative_number
+    raise "Negative numbers not allowed -> #{negatives.join(', ')}" if negatives.any?
+  end
+
+  private def negatives
+    @negatives ||= digits.select { |digit| digit.negative? }
   end
 end
